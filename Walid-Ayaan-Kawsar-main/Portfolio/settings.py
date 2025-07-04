1d6d2af4b762f58dc70127e142e81665936ab343
Walid Ayaan Kawsar <walidayaankawsar@gmail.com>
2025-07-04 07:10:48 -0900

    updated

--- Walid-Ayaan-Kawsar-main/Portfolio/settings.py
+++ Walid-Ayaan-Kawsar-main/Portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-&t5$cketqnql#@zmgm-()w4lp^t-sd^l9_9=h-moqp7--#o-_f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['walid-ayaan-kawsar.onrender.com', 'localhost', 'localhost:8000']
+ALLOWED_HOSTS = ['localhost', 'localhost:8000']
 
 
 # Application definition
